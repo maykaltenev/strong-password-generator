8c4221dcae753ef36c9e68f14dba0191b73f89b8
maykaltenev <michaeltenev@gmail.com>
2022-06-02 21:24:57 +0200

    generate from input

--- app.js
+++ app.js
@@ -4,28 +4,63 @@ let capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let number = '0123456789';
 let divPassword = document.querySelector('#password');
 let button = document.querySelector('#btn');
-
+let newValue = document.querySelector('#domTextElement');
 // document.getElementById("valueInput").innerHTML = inputValue;
-let submit = document.querySelector("input[type='submit']");
+
 let body = document.querySelector('body');
 let main = document.querySelector('main')
 let container = document.querySelector('.container');
 let text = document.querySelectorAll('.text');
+let form = document.querySelector('form');
+let newInput = [];
+// let getValue = (e) => {
+//     e.preventDefault()
+//     let submit = document.querySelector('#domTextElement').value;
+//     newInput.push(submit);
+//     document.querySelector('#domTextElement').value = ''
+// }
 
-// submit.addEventListener('click', () => {
-//     some = document.getElementById("domTextElement").value;
-//     console.log(some)
-// })
+// console.log(newInput)
+// console.log(newInput[0])
+// form.addEventListener('submit', getValue)
+// // console.log(some);
+// const randomPassword = () => {
+//     let result = [];
 
-// console.log(some);
-const randomPassword = () => {
+//     // if (length[0] < 8) {
+//     //     return `Password is too short please provide a number between 8 and 20!`;
+//     // } else if (length[0] > 20) {
+//     //     return `Password is too long please provide a number between 8 and 20!`
+//     // } else {
+//     for (let i = 0; i < newInput[0]; i++) {
+//         if (i % 2 === 0) {
+//             result.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)])
+//         } else if (i % 3 === 0) {
+//             result.push(smallLetters[Math.floor(Math.random() * smallLetters.length)])
+//         } else if (i % 5 === 0) {
+//             result.push(symbols[Math.floor(Math.random() * symbols.length)]);
+//         } else {
+//             result.push(number[Math.floor(Math.random() * number.length)]);
+//         }
+//         divPassword.innerHTML = result.reverse().join('');
+//         return result.reverse().join('');
+//     }
+
+// }
+//-----version 2
+
+let getValue = (e) => {
+    e.preventDefault()
     let result = [];
-    // if (inputValue < 8) {
+    let newValue = document.querySelector('#domTextElement').value;
+    let parsedValue = Number(newValue);
+    console.log(parsedValue)
+    // if (parsedValue < 8) {
     //     return `Password is too short please provide a number between 8 and 20!`;
-    // } else if (inputValue > 20) {
+    // } else if (parsedValue) {
     //     return `Password is too long please provide a number between 8 and 20!`
     // } else {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < parsedValue; i++) {
         if (i % 2 === 0) {
             result.push(capitalLetters[Math.floor(Math.random() * capitalLetters.length)])
         } else if (i % 3 === 0) {
@@ -36,10 +71,15 @@ const randomPassword = () => {
             result.push(number[Math.floor(Math.random() * number.length)]);
         }
     }
-
-    divPassword.innerHTML = result.reverse().join('');
+    document.querySelector('#domTextElement').value = ''
+    divPassword.textContent = result.reverse().join('');
     return result.reverse().join('');
+
+
 }
+
+
+form.addEventListener('submit', getValue)
 
 body.style.background = '#038C8C';
 body.style.display = 'flex';
@@ -76,13 +116,15 @@ container.style.fontSize = '1.5rem'
 container.style.borderTop = '0.5rem #6B00DE solid'
 container.style.boxShadow = '0px 0px 10px white'
 divPassword.style.color = '#62F700'
-button.style.padding = '12px 24px';
+button.style.padding = '6px 20px';
 button.style.cursor = 'pointer';
 button.style.fontSize = '15px'
 button.style.fontWeight = '500'
 button.style.color = 'white'
 button.style.background = '#6B00DE'
 button.style.boxShadow = '0px 0px 5px white'
+newValue.style.padding = '6px 20px';
 
-button.addEventListener('click', randomPassword)
+newValue.style.fontSize = '1rem'
+
 
